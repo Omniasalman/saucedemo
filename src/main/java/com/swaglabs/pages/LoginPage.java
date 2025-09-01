ade1da78b26ca4d486f6d55ebf8f71472fbd228f
omnia salman <omniasalman928@gmail.com>
2025-09-01 12:35:06 +0300

    Initial commie

---
+++ src/main/java/com/swaglabs/pages/LoginPage.java
@@ -0,0 +1,18 @@
+package com.swaglabs.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    //Locators
+    private final WebDriver driver;
+    //Constructor
+    public LoginPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+
+
+    //Action
+
+    //Validation
+}
